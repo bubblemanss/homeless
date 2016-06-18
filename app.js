133b805cf609e2810b923dd8af96d941a1fa023d
Maidi Lin <m39lin@uwaterloo.ca>
2016-06-17 22:16:52 -0700

    initial server

---
+++ app.js
@@ -0,0 +1,48 @@
+var express = require("express");
+//var bodyParser = require("body-parser");
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+var server = app.listen(app.get("port"), function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
+
+// Configuration
+
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'jade');
+// app.use(express.methodOverride());
+// app.use(app.router);
+// app.use(express.static(__dirname + '/public'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded());
+
+// Routes
+
+app.get('/homes', function(req, res){
+    res.set({
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+    });
+
+    var data = {
+        latitude: 1,
+        longitude: 2,
+        name: "home"
+    }
+
+    res.status(200).send(data);
+});
+
+app.post('/location', function(req, res){
+    res.set({
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+    });
+
+    res.status(200).send("here");
+});
